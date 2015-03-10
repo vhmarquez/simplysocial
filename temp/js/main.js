ab8e172aa9d997ad858dda7345c6fa53112e99d1
Victor Marquez <victormarquez.dev@gmail.com>
2015-03-09 23:41:22 -0400

    Sprites, status

--- temp/js/main.js
+++ temp/js/main.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
+
+	// Masonry Grid View
 	var	container = $('#streamGrid');
-	var	msnry = new Masonry( container, {
+	var	msnry = new Masonry( container[0], {
 		itemSelector: '.stream-grid-item'
+	});
+
+	// View Switch
+	$('.view-tab').each(function(){
+		var tab = $(this).attr('href');
+
+		console.log(tab);
+		
+		$(this).on('click', function(event) {
+			event.preventDefault();
+
+			$('.view-tab .icon-list, .view-tab .icon-grid').removeClass('active');
+			$(this).children('.icon-list, .icon-grid').addClass('active');
+
+			$('.view-display').removeClass('active');
+			$(tab).addClass('active');
+		});
+
 	});
 });
